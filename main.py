af04462aaf43bec1bcc14e99b1aeebfcadacfbf7
alexandr rodionov <alexandr@alexandrs-MacBook-Pro.local>
2025-05-29 16:04:46 +0300

    Initial commit: Calculator project with modal functionality

---
+++ main.py
@@ -0,0 +1,9 @@
+
+product=input("Выберите тип продукта. Нажмите 1, если Segment Scoring, 2 - Retargeting, 3 - Call Center")
+subscribe=input("Выберите тип подписки. 1 - Tech, 2 - Must, 3 - Pro")
+average_bill=int(input("Укажите средний чек"))
+contacts=int(input("Укажите количество контактов"))
+
+
+
+def budget(product,subscribe, average_bill,contacts):
